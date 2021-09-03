bf95bcc08c2806ee57bd8b762531327666dcd2d9
a.k.a.a.h.l <a.k.afiq@live.com>
2021-09-03 21:41:54 +0800

    Added modal deactivation

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,9 +7,18 @@ const backdrop = {
   hidden: { opacity: 0 },
 };
 
+const modal = {
+  hidden: { y: "-100vh", opacity: 0 },
+  visible: {
+    y: "200px",
+    opacity: 1,
+    transition: { delay: 0.5 },
+  },
+};
+
 const Modal = ({ showModal, setShowModal }) => {
   return (
-    <AnimatePresence exitBeforeEnter>
+    <AnimatePresence>
       {showModal && (
         <motion.div
           className="backdrop"
@@ -17,7 +26,14 @@ const Modal = ({ showModal, setShowModal }) => {
           initial="hidden"
           animate="visible"
           exit="hidden"
-        ></motion.div>
+        >
+          <motion.div className="modal" variants={modal}>
+            <p>Want to make another Pizza?</p>
+            <Link to="/">
+              <button onClick={() => setShowModal(false)}>Start Again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
